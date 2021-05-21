20f3b7f2ab0792b0c44d00cab5bc5125f25d49b1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-05-21 18:37:34 +0200

    Add task to npm runner example

--- pinefile-npm.js
+++ pinefile-npm.js
@@ -1,5 +1,11 @@
-const { configure } = require('@pinefile/pine');
+const { configure, log } = require('@pinefile/pine');
 
 configure({
   runner: '@pinefile/runner-npm',
 });
+
+module.exports = {
+  hello: () => {
+    log.info('hello task is not in package.json');
+  },
+};
