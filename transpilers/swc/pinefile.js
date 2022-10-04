947cd6429641c83c6a16038a1cf7f1622a6d54e6
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2022-10-04 22:38:14 +0200

    Clean up swc example

--- transpilers/swc/pinefile.js
+++ transpilers/swc/pinefile.js
@@ -2,7 +2,4 @@ import { getConfig } from '@pinefile/pine';
 
 export default async () => {
   console.log('Running with @swc-node/register', getConfig());
-  console.log(
-    arguments ///.some((x) => x.indexOf('swc-node'))
-  );
 };
